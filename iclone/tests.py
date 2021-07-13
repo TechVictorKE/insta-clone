81fa98cf1cd1d652d96bdc4503b20b64e6066743
TechVictorKE <vicikibocha@gmail.com>
2021-07-13 13:44:00 +0300

    Created tests

--- iclone/tests.py
+++ iclone/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
-
+from .models import Profile, Image, Comment, Follow
 # Create your tests here.
+class FollowTestClass(TestCase):
+    def setUp(self):
+        self.moringa=Follow(username='moringa',followed='kibocha')
+                            
+    def test_instance(self):
+        self.assertTrue(isinstance(self.moringa,Follow))
+
+class CommentTestClass(TestCase):
+    def setUp(self):
+        self.first=Comment(post=1,
+                            username='moringa',
+                            comment='Whole lotta gang shit.',
+                            date='Jul 13, 2021, 13:38 p.m.',
+                            count=0)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.first,Comment))
