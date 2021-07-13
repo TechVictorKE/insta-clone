3f0c5eb3aaaba234f711f39d9e693698d5e01cfe
TechVictorKE <vicikibocha@gmail.com>
2021-07-13 18:31:17 +0300

    Configured settings.py for heroku deployment

--- instagram/settings.py
+++ instagram/settings.py
@@ -136,4 +136,7 @@ DATABASES['default'].update(db_from_env)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static"), ] 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Configure Django App for Heroku.
+django_on_heroku.settings(locals())
